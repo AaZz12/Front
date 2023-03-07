b90dcc8948cee05d1c13fc4836d216165278d5c1
AaZz <theaazz@outlook.com>
2023-03-07 10:47:29 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ const app=express(); //init de l'app web
 const port=3000; //port sur lequel le serv web est lancé
 function main(){
     app.get("/",(req, res)=>{
-        res.send("Hello world");
+        const dt=new Date();
+        res.send("Hello world"+dt);
+        console.log(dt);
     })
     app.listen(port, function(){
         console.log(`serveur lancé sur port ${port}`);
     });
 }
+main();
