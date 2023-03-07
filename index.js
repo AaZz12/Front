a1543335aa37eb2fef8f6eb91b5c1646c33d2c8a
AaZz <theaazz@outlook.com>
2023-03-07 11:38:56 +0100

    html

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
-const express=require('express'); 
-const app=express(); //init de l'app web
-const port=3000; //port sur lequel le serv web est lancé
-function main(){
-    app.get('/',(req, res)=>{
-        const dt=new Date();
-        res.send('Hello world'+dt);
-        console.log(dt);
-    });
-    app.listen(port, function(){
-        console.log(`serveur lancé sur port ${port}`);
-    });
-}
-main();
+
+
+// --------------- MODULES IMPORT ---------------
+
+
+const express = require('express');
+
+
+// --------------- SERVER SETUP ---------------
+
+
+const app = express();
+const port = 3000;  // Localhost Port
+
+app.listen(port, () => {
+    console.log(`App listening to port ${port}`);
+});
+
+app.use(express.static('public'));
